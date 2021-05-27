de040688874f5a4e9e5ba780b6e701c4ce7b2fe2
John Foster <johnfost.361@gmail.com>
2021-05-27 15:29:27 -0400

    WIP

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("test")
+import movie
+from requests import get
+from bs4 import BeautifulSoup
+
+url = 'https://www.imdb.com/search/title/'
+
+# just get the top 50 for now (it appears on the page that opens up first)
+
+# html class tags:
+#     title: <a href="/title/stuff"> title of movie
+#     year: <span class="lister-item-year text-muted unbold">(2021)</span>
+#     rating: <div class="inline-block ratings-imdb-rating" name="ir" data-value="5.9">
+#         <span class="global-sprite rating-star imdb-rating"></span>
+#         <strong>5.9</strong>
+#         </div>
+#     description: <p class="text-muted">description here</p>
+
+# genre = input("Type a movie genre \n")
+# minRating, maxRating = input("Enter a range of ratings (separated by a dash) \n").split('-')
+# minYear, maxYear = input("Enter a year range (separated by a dash)  \n").split('-')
+
+response = get(url)
+
+# obj = movie.movie("movie name", 1990, 5.0, "This is what the movie is about")
+# obj.print_info()
---
+++ movie.py
@@ -0,0 +1,15 @@
+class movie:
+    name = 0
+    year = 0
+    rating = 0
+    description = 0
+
+    def __init__(self, name, year, rating, description):
+        self.name = name
+        self.year = year
+        self.rating = rating
+        self.description = description
+
+    def print_info(self):
+        print("Name: " + self.name + "\n" + "Year: " + str(self.year) + "\n" + "Rating: " + str(self.rating) + "\n" +
+              "Description: " + self.description)
